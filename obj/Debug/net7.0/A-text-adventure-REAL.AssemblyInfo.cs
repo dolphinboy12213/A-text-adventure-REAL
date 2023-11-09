ae1cd17e98fdfd65d78f9e9d185a942994fdf829
TheDolphin <dolphinboy12213@gmail.com>
2023-11-09 17:01:05 -0600

    id even know
    hehe

--- obj/Debug/net7.0/A-text-adventure-REAL.AssemblyInfo.cs
+++ obj/Debug/net7.0/A-text-adventure-REAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A-text-adventure-REAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd104786a7c96d4f40268baa021ee004cc3b8459")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("A-text-adventure-REAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A-text-adventure-REAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
